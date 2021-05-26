c53706d4b3a671295c13dc8e022ac0d5d2b33f87
Douglas Javier Colque Mullisaca <49354025+DouglasJavier@users.noreply.github.com>
2021-05-25 23:48:42 -0400

    Add files via upload
    Este código fue desarrollado en C# con el IDE visual studio
     con la utilización de estructuras, clasifica en dicha imagen al menos 3 segmentos.

---
+++ Ejercicio3Parcial2/Ejercicio3Parcial2/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace Ejercicio3Parcial2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CARGAR IMAGEN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 60);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 35);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "AGUA";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 102);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(119, 33);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "TIERRA";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(13, 141);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(119, 31);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "VEGETACIÓN";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(138, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(884, 571);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 178);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(120, 26);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "NEVADO";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(13, 210);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(119, 27);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "REINICIAR";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1034, 595);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Ejercicio3Parcial2/Ejercicio3Parcial2/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio3Parcial2
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            Bitmap bmp = new Bitmap(openFileDialog1.FileName);
+            pictureBox1.Image = bmp;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(pictureBox1.Image);
+            Bitmap bmp2 = new Bitmap(bmp.Width, bmp.Height);
+            int cRto, cGto, cBto;
+            Color c = new Color();
+            for (int i = 0; i < bmp.Width -3; i = i + 3)
+                for (int j = 0; j < bmp.Height - 3; j = j + 3)
+                {
+
+                    cRto = 0; cGto = 0; cBto = 0;
+                    for (int k = i; k < i + 3; k++)
+                        for (int l = j; l < j + 3; l++)
+                        {
+                            c = bmp.GetPixel(k, l);
+                            cRto = c.R + cRto; cGto = c.G + cGto; cBto = c.B + cBto;
+                        }
+                    cRto = cRto / 9;
+                    cGto = cGto / 9;
+                    cBto = cBto / 9;
+                    c = bmp.GetPixel(i, j);
+                   // if (((0 <= cRto) && (cRto <= 35)) && ((cGto<= 74) && (6 <= cGto)) && ((cBto<= 255) && (24 <= cBto))&&(cGto<cBto))
+                   if((cRto<=cGto)&&(cGto<=cBto))
+                        for (int k = i; k < i + 3; k++)
+                            for (int l = j; l < j + 3; l++)
+                            {
+                                bmp2.SetPixel(k, l, Color.Black);
+                            }
+                    else
+                        for (int k = i; k < i + 3; k++)
+                            for (int l = j; l < j + 3; l++)
+                            {
+                                c = bmp.GetPixel(k, l);
+                                bmp2.SetPixel(k, l, c);
+                            }
+                }
+            pictureBox1.Image = bmp2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+             bmp = new Bitmap(pictureBox1.Image);
+            Bitmap bmp2 = new Bitmap(bmp.Width, bmp.Height);
+            int cRto, cGto, cBto;
+            Color c = new Color();
+            for (int i = 0; i < bmp.Width - 3; i = i + 3)
+                for (int j = 0; j < bmp.Height - 3; j = j + 3)
+                {
+
+                    cRto = 0; cGto = 0; cBto = 0;
+                    for (int k = i; k < i + 3; k++)
+                        for (int l = j; l < j + 3; l++)
+                        {
+                            c = bmp.GetPixel(k, l);
+                            cRto = c.R + cRto; cGto = c.G + cGto; cBto = c.B + cBto;
+                        }
+                    cRto = cRto / 9;
+                    cGto = cGto / 9;
+                    cBto = cBto / 9;
+                    c = bmp.GetPixel(i, j);
+                    // if (((0 <= cRto) && (cRto <= 35)) && ((cGto<= 74) && (6 <= cGto)) && ((cBto<= 255) && (24 <= cBto))&&(cGto<cBto))
+                    if ((cRto >= cGto) && (cGto >= cBto))
+                        for (int k = i; k < i + 3; k++)
+                            for (int l = j; l < j + 3; l++)
+                            {
+                                bmp2.SetPixel(k, l, Color.Black);
+                            }
+                    else
+                        for (int k = i; k < i + 3; k++)
+                            for (int l = j; l < j + 3; l++)
+                            {
+                                c = bmp.GetPixel(k, l);
+                                bmp2.SetPixel(k, l, c);
+                            }
+                }
+            pictureBox1.Image = bmp2;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+             bmp = new Bitmap(pictureBox1.Image);
+            Bitmap bmp2 = new Bitmap(bmp.Width, bmp.Height);
+            int cRto, cGto, cBto;
+            Color c = new Color();
+            for (int i = 0; i < bmp.Width - 3; i = i + 3)
+                for (int j = 0; j < bmp.Height - 3; j = j + 3)
+                {
+
+                    cRto = 0; cGto = 0; cBto = 0;
+                    for (int k = i; k < i + 3; k++)
+                        for (int l = j; l < j + 3; l++)
+                        {
+                            c = bmp.GetPixel(k, l);
+                            cRto = c.R + cRto; cGto = c.G + cGto; cBto = c.B + cBto;
+                        }
+                    cRto = cRto / 9;
+                    cGto = cGto / 9;
+                    cBto = cBto / 9;
+                    c = bmp.GetPixel(i, j);
+                    // if (((0 <= cRto) && (cRto <= 35)) && ((cGto<= 74) && (6 <= cGto)) && ((cBto<= 255) && (24 <= cBto))&&(cGto<cBto))
+                    if ((cRto< cBto) && (cGto >= cBto))
+                        for (int k = i; k < i + 3; k++)
+                            for (int l = j; l < j + 3; l++)
+                            {
+                                bmp2.SetPixel(k, l, Color.Black);
+                            }
+                    else
+                        for (int k = i; k < i + 3; k++)
+                            for (int l = j; l < j + 3; l++)
+                            {
+                                c = bmp.GetPixel(k, l);
+                                bmp2.SetPixel(k, l, c);
+                            }
+                }
+            pictureBox1.Image = bmp2;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(pictureBox1.Image);
+            Bitmap bmp2 = new Bitmap(bmp.Width, bmp.Height);
+            int cRto, cGto, cBto;
+            Color c = new Color();
+            for (int i = 0; i < bmp.Width - 3; i = i + 3)
+                for (int j = 0; j < bmp.Height - 3; j = j + 3)
+                {
+
+                    cRto = 0; cGto = 0; cBto = 0;
+                    for (int k = i; k < i + 3; k++)
+                        for (int l = j; l < j + 3; l++)
+                        {
+                            c = bmp.GetPixel(k, l);
+                            cRto = c.R + cRto; cGto = c.G + cGto; cBto = c.B + cBto;
+                        }
+                    cRto = cRto / 9;
+                    cGto = cGto / 9;
+                    cBto = cBto / 9;
+                    c = bmp.GetPixel(i, j);
+                    // if (((0 <= cRto) && (cRto <= 35)) && ((cGto<= 74) && (6 <= cGto)) && ((cBto<= 255) && (24 <= cBto))&&(cGto<cBto))
+                    if ((cRto >=200) && (cGto >= 200)&&(cBto>=200))
+                        for (int k = i; k < i + 3; k++)
+                            for (int l = j; l < j + 3; l++)
+                            {
+                                bmp2.SetPixel(k, l, Color.Black);
+                            }
+                    else
+                        for (int k = i; k < i + 3; k++)
+                            for (int l = j; l < j + 3; l++)
+                            {
+                                c = bmp.GetPixel(k, l);
+                                bmp2.SetPixel(k, l, c);
+                            }
+                }
+            pictureBox1.Image = bmp2;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = bmp;
+        }
+    }
+}
